c4902beb655ae3a04aeeac3e0d314bf3b65444a2
MedericFourmy <mederic.fourmy@gmail.com>
2023-08-01 06:26:40 -0400

    Fix a few issues with cnos check script

--- check_cnos_submission.py
+++ check_cnos_submission.py
@@ -14,13 +14,35 @@ CNOS_SUBMISSION_FILES = {
     "tless": 'baseline-sam-dinov2-blenderproc4bop_tless-test_3305b238-3d93-4954-81ba-3ff3786265d9.json',
     "tudl": 'baseline-sam-dinov2-blenderproc4bop_tudl-test_c6cd05c1-89a1-4fe5-88b9-c1b57ef15694.json',
     "icbin": 'baseline-sam-dinov2-blenderproc4bop_icbin-test_f58b6868-7e70-4ab2-9332-65220849f8c1.json',
-    "itodd": 'baseline-sam-dinov2-blenderproc4bop_itodd-test_82442e08-1e79-4f54-8e88-7ad6b986dd96.json',
-    "hb": 'baseline-sam-dinov2-blenderproc4bop_hb-test_f32286f9-05f5-4123-862f-18f00e67e685.json',
+    # "itodd": 'baseline-sam-dinov2-blenderproc4bop_itodd-test_82442e08-1e79-4f54-8e88-7ad6b986dd96.json',
+    # "hb": 'baseline-sam-dinov2-blenderproc4bop_hb-test_f32286f9-05f5-4123-862f-18f00e67e685.json',
+}
+
+TEST_DIRS = {
+    'ycbv': 'test',
+    'lmo': 'test',
+    'tless': 'test_primesense',
+    'tudl': 'test',
+    'icbin': 'test',
+    'itodd': 'test',
+    'hb': 'test_primesense',
+}
+
+IMG_DIRS = {
+    'ycbv': 'rgb',
+    'lmo': 'rgb',
+    'tless': 'rgb',
+    'tudl': 'rgb',
+    'icbin': 'rgb',
+    'itodd': 'gray',
+    'hb': 'rgb',
 }
 
 SHOW = False
 
 for ds_name in CNOS_SUBMISSION_FILES:
+
+    print(f'\n### {ds_name} ###')
 
     detections_path = CNOS_SUBMISSION_DIR / CNOS_SUBMISSION_FILES[ds_name]
 
@@ -43,7 +65,7 @@ for ds_name in CNOS_SUBMISSION_FILES:
     #################
     # Get all image ids for each scene id from ds test
     #################
-    test_dir = ds_bop_dir / 'test'
+    test_dir = ds_bop_dir / TEST_DIRS[ds_name]
     all_scene_ids = [d.name for d in test_dir.iterdir()]
 
     test_targets = json.loads(test_targets_path.read_text())
@@ -54,7 +76,7 @@ for ds_name in CNOS_SUBMISSION_FILES:
     scene_nb_obj = {}
     for sid_str in all_scene_ids:
 
-        rgb_dir = test_dir / sid_str / Path('rgb')
+        img_dir = test_dir / sid_str / Path(IMG_DIRS[ds_name])
 
         sid = int(sid_str)
 
@@ -62,7 +84,7 @@ for ds_name in CNOS_SUBMISSION_FILES:
         img_ids_subtest = df_test_targets[df_test_targets['scene_id'] == sid]['im_id'].unique()
 
         # retrieve dataset image ids and filter them
-        all_img_ids_for_sid = sorted([int(f.stem) for f in rgb_dir.glob('*')])
+        all_img_ids_for_sid = sorted([int(f.stem) for f in img_dir.glob('*')])
         scene_image_ids[sid] = [iid for iid in all_img_ids_for_sid if iid in img_ids_subtest]
         # scene_image_ids[sid] = all_img_ids_for_sid
 
@@ -85,24 +107,14 @@ for ds_name in CNOS_SUBMISSION_FILES:
     all_dets = json.loads(detections_path.read_text())
     df_all_dets = pd.DataFrame.from_records(all_dets)
 
-    # print('scene ids:', df_all_dets['scene_id'].unique())
-    # print('nb_scene_id: ', df_all_dets['scene_id'].nunique())
-    # print('nb_image_id: ', df_all_dets['image_id'].nunique())
-    # print('nb_category_id: ', df_all_dets['category_id'].nunique())
-
-    df_unique_det = df_all_dets.groupby('scene_id')['image_id'].nunique()
-
     df_image_id = df_all_dets.groupby('scene_id')['image_id'].unique()
     scene_image_ids_cnos = df_image_id.to_dict()
 
-
     ### NB unique images vs nb images with detection
-    ycbv_nb_images = sum(len(ids) for ids in scene_image_ids.values())
+    ds_nb_images = sum(len(ids) for ids in scene_image_ids.values())
     cnos_nb_images_with_det = sum(len(ids) for ids in scene_image_ids_cnos.values())
 
-    print('ycbv_nb_images: ', ycbv_nb_images)
-    print('cnos_nb_images_with_det: ', cnos_nb_images_with_det)
-    print('% img with CNOS detection: ', 100*(cnos_nb_images_with_det/ycbv_nb_images))
+    print(f'# image cnos/{ds_name}: {cnos_nb_images_with_det}/{ds_nb_images}')
 
     # which submissions are missing??
     d_missing = {}
@@ -124,12 +136,15 @@ for ds_name in CNOS_SUBMISSION_FILES:
     cat_ids_per_sid_iid = df_all_dets.groupby(['scene_id', 'image_id']).category_id.nunique()
     all_obj_nb = cat_ids_per_sid_iid.to_list()
 
-    plt.figure('Detection numbers')
+    ##################
+    plt.figure()
     plt.title(f'Number of detections per image {ds_name}')
     plt.boxplot(all_obj_nb)
+    fig_name = f'cnos_nb_img_{ds_name}.png'
+    print('Saving', fig_name)
+    plt.savefig(fig_name)
 
-
-
+    ##################
     scene_ids = list(scene_image_ids_cnos.keys())
     markersize = 2
     plt.figure()
@@ -142,8 +157,8 @@ for ds_name in CNOS_SUBMISSION_FILES:
     plt.xlabel('Scene id')
     plt.ylabel('Image ids')
     # plt.legend()
-    fig_name = f'cnos_dets_{ds_name}'
-    print('Saving ', fig_name)
+    fig_name = f'cnos_dets_{ds_name}.png'
+    print('Saving', fig_name)
     plt.savefig(fig_name)
 
 if SHOW:
