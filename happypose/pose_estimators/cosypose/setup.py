a45008c42ff6d8479ff099a4e308c142bcfbc1bb
Guilhem Saurel <guilhem.saurel@laas.fr>
2023-09-26 14:09:06 +0200

    fix build without conda

--- happypose/pose_estimators/cosypose/setup.py
+++ happypose/pose_estimators/cosypose/setup.py
@@ -1,29 +1,27 @@
 import os
-from setuptools import setup, find_packages
-from torch.utils.cpp_extension import BuildExtension, CppExtension
 from os import path
+
+from setuptools import find_packages, setup
+from torch.utils.cpp_extension import BuildExtension, CppExtension
 
 here = path.abspath(path.dirname(__file__))
 
 # Use correct conda compiler used to build pytorch
-os.environ['CXX'] = os.environ.get('GXX', '')
+if "GXX" in os.environ:
+    os.environ["CXX"] = os.environ.get("GXX", "")
 
 setup(
-    name='cosypose',
-    version='1.0.0',
-    description='CosyPose',
+    name="cosypose",
+    version="1.0.0",
+    description="CosyPose",
     packages=find_packages(),
     ext_modules=[
         CppExtension(
-            name='cosypose_cext',
-            sources=[
-                'cosypose/csrc/cosypose_cext.cpp'
-            ],
-            extra_compile_args=['-O3'],
-            verbose=True
+            name="cosypose_cext",
+            sources=["cosypose/csrc/cosypose_cext.cpp"],
+            extra_compile_args=["-O3"],
+            verbose=True,
         )
     ],
-    cmdclass={
-        'build_ext': BuildExtension
-    }
+    cmdclass={"build_ext": BuildExtension},
 )
