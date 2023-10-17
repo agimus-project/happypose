3343d945c01d7764697d37865854c503bdff6875
Guilhem Saurel <guilhem.saurel@laas.fr>
2023-10-17 12:02:16 +0200

    ci: test poetry

--- happypose/pose_estimators/cosypose/setup.py
+++ happypose/pose_estimators/cosypose/setup.py
@@ -1,27 +1,16 @@
-import os
-from os import path
+from pybind11.setup_helpers import Pybind11Extension, build_ext
+from setuptools import setup
 
-from setuptools import find_packages, setup
-from torch.utils.cpp_extension import BuildExtension, CppExtension
-
-here = path.abspath(path.dirname(__file__))
-
-# Use correct conda compiler used to build pytorch
-if "GXX" in os.environ:
-    os.environ["CXX"] = os.environ.get("GXX", "")
+ext_modules = [
+    Pybind11Extension("cosypose_cext", ["cosypose/csrc/cosypose_cext.cpp"]),
+]
 
 setup(
     name="cosypose",
     version="1.0.0",
     description="CosyPose",
-    packages=find_packages(),
-    ext_modules=[
-        CppExtension(
-            name="cosypose_cext",
-            sources=["cosypose/csrc/cosypose_cext.cpp"],
-            extra_compile_args=["-O3"],
-            verbose=True,
-        )
-    ],
-    cmdclass={"build_ext": BuildExtension},
+    ext_modules=ext_modules,
+    cmdclass={"build_ext": build_ext},
+    zip_safe=False,
+    python_requires=">=3.9",
 )
