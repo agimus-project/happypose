0806b53722e6fc80bf7bcbbec86bfe940681d708
Guilhem Saurel <guilhem.saurel@laas.fr>
2025-01-30 11:01:02 +0100

    devShell

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Description for the project";
+  description = "Toolbox and trackers for object pose-estimation";
 
   inputs = {
     flake-parts.url = "github:hercules-ci/flake-parts";
@@ -9,13 +9,6 @@
   outputs =
     inputs@{ flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
-      imports = [
-        # To import a flake module
-        # 1. Add foo to inputs
-        # 2. Add foo as a parameter to the outputs function
-        # 3. Add here: foo.flakeModule
-
-      ];
       systems = [
         "x86_64-linux"
         "aarch64-linux"
@@ -23,27 +16,14 @@
         "x86_64-darwin"
       ];
       perSystem =
+        { pkgs, ...  }:
         {
-          config,
-          self',
-          inputs',
-          pkgs,
-          system,
-          ...
-        }:
-        {
-          # Per-system attributes can be defined here. The self' and inputs'
-          # module parameters provide easy access to attributes of the same
-          # system.
-
-          # Equivalent to  inputs'.nixpkgs.legacyPackages.hello;
-          packages.default = pkgs.hello;
+          devShells.default = pkgs.mkShell {
+            packages = with pkgs; [
+              python3
+              uv
+            ];
+          };
         };
-      flake = {
-        # The usual flake attributes can be defined here, including system-
-        # agnostic ones like nixosModule and system-enumerating ones, although
-        # those are more easily expressed in perSystem.
-
-      };
     };
 }
